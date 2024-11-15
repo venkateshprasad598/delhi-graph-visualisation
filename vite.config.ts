7a6fa62f92df88005c465872c3d385649de28f4a
venkateshprasad598 <venkateshprasad598@gmail.com>
2024-11-16 00:43:56 +0530

    Graph microfrontend impemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation';
-
+// import federation from '@originjs/vite-plugin-federation';
 
 export default defineConfig({
   plugins: [
     react(),
-    federation({
-      name: 'remote-app',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './App': './src/App.tsx',
-      },
-      shared: ['react', 'react-dom'],
-    }),
+    // federation({
+    //   name: 'remote-app',
+    //   filename: 'remoteEntry.js',
+    //   exposes: {
+    //     './App': './src/App.tsx',
+    //   },
+    //   shared: ['react', 'react-dom'],
+    // }),
   ],
   build: {
     modulePreload: false,
